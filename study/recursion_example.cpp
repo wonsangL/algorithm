7d7b6234d7e26b098168b9b38970f53ea2a7c26b
wonsangL <harryandws@gmail.com>
2018-06-21 22:26:15 +0900

    recurstion example p.149

--- study/recursion_example.cpp
+++ study/recursion_example.cpp
@@ -5,12 +5,35 @@ using namespace std;
 
 void recursion(int n, int count, vector<int> list) {
 	if (count == 0) {
+		cout << "(";
 		for (vector<int>::iterator it = list.begin(); it != list.end(); it++) {
-			cout << it << endl;
+			cout << *it << " ";
+		}
+		cout << ")" << endl;
+		
+		return;
+	}
+
+	if (list.empty()) {
+		for (int i = 0; i <= n - count; i++) {
+			list.push_back(i + 1);
+			recursion(n, count - 1, list);
+			list.pop_back();
+		}
+	}
+	else {
+		for (int i = list[list.size() - 1]; i < n; i++) {
+			list.push_back(i + 1);
+			recursion(n, count - 1, list);
+			list.pop_back();
 		}
 	}
 }
 
 int main() {
+	vector<int> list;
+	int n, count;
+	cin >> n >> count;
 
+	recursion(n, count, list);
 }
